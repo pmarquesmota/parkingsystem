51b51824be80dec9b3dc25d038f1564ed53b3c77
Paul Marques Mota <paul@sas-kalliste.fr>
2021-07-20 17:02:03 +0200

    fix the CheckStyle errors

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -1,14 +1,30 @@
 package com.parkit.parkingsystem.constants;
 
 public final class DBConstants {
+    /**
+     * A dummy constructor for CheckStyle.
+     */
+    private DBConstants() {
+    }
 
+    /**
+     * The prepared SQL statement for getting the next parking spot
+     * from the database.
+     */
     public static final String GET_NEXT_PARKING_SPOT =
             "select min(PARKING_NUMBER) "
                     + "from parking "
                     + "where AVAILABLE = true and TYPE = ?";
+
+    /**
+     * The prepared SQL statement for updating a parking spot in the database.
+     */
     public static final String UPDATE_PARKING_SPOT =
             "update parking set available = ? where PARKING_NUMBER = ?";
 
+    /**
+     * The prepared SQL statement for saving a ticket in the database.
+     */
     public static final String SAVE_TICKET =
             "insert into ticket("
                     + "PARKING_NUMBER, "
@@ -17,8 +33,16 @@ public final class DBConstants {
                     + "IN_TIME, "
                     + "OUT_TIME) "
                     + "values(?,?,?,?,?)";
+
+    /**
+     * The prepared statement for updating a ticket.
+     */
     public static final String UPDATE_TICKET =
             "update ticket set PRICE=?, OUT_TIME=? where ID=?";
+
+    /**
+     * The prepared SQL statement for getting a ticket.
+     */
     public static final String GET_TICKET =
             "select t.PARKING_NUMBER, "
                     + "t.ID, "
@@ -28,7 +52,7 @@ public final class DBConstants {
                     + "p.TYPE "
                     + "from ticket t,parking p "
                     + "where p.parking_number = t.parking_number "
-                       + "and t.VEHICLE_REG_NUMBER=? "
+                    + "and t.VEHICLE_REG_NUMBER=? "
                     + "order by t.IN_TIME "
                     + "limit 1";
 }
