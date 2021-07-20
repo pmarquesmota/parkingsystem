83cb07af5bb681e85d9694d36966b4af307e5e44
Paul Marques Mota <paul@sas-kalliste.fr>
2021-07-20 12:05:22 +0200

    fix javadoc for App.java

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -4,19 +4,19 @@ import com.parkit.parkingsystem.service.InteractiveShell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 /**
-* The main class of the programm
+* The main class of the programm.
 * @author openclassrooms
  */
  final class App {
      /**
-    * Used to initialize the log4J system
+    * Used to initialize the log4J system.
     * @author openclassrooms
     */
     private static final Logger LOGGER = LogManager.getLogger("App");
 
    /**
-   * The main class, launches the project
-   * @parameter String the arguments of the program
+   * The main class, launches the project.
+   * @param args the arguments of the program
    */
    private static  void main(final String[] args) {
         LOGGER.info("Initializing Parking System");
