0219111dddaf85e3f542d8fe82ea55ed41d6dbd7
Paul Marques Mota <paul@sas-kalliste.fr>
2021-07-20 17:06:00 +0200

    fix the CheckStyle errors

--- src/main/java/com/parkit/parkingsystem/constants/Fare.java
+++ src/main/java/com/parkit/parkingsystem/constants/Fare.java
@@ -1,7 +1,16 @@
 package com.parkit.parkingsystem.constants;
 
-public class Fare {
-    private Fare(){}
+public final class Fare {
+    private Fare() {
+    }
+
+    /**
+     * The rate for the bike.
+     */
     public static final double BIKE_RATE_PER_HOUR = 1.0;
+
+    /**
+     * The rate for the car.
+     */
     public static final double CAR_RATE_PER_HOUR = 1.5;
 }
